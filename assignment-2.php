cc9ab01d3d354bf7a66393ce4386a9e0a9542161
Ashrafi <ashrafikhanam.18@gmail.com>
2023-02-06 17:42:11 +0600

    Comission Calculator

--- assignment-2.php
+++ assignment-2.php
@@ -1,4 +1,5 @@
 <?php
+//Comission calculator
 
 function calculate_commission($tuitionFee) {
   return $tuitionFee>= 20000 ? 0.25 * $tuitionFee : ($tuitionFee>= 10000 ? 0.2 * $tuitionFee : ($tuitionFee >= 7000 ? 0.15 * $tuitionFee : "Invalid data"));
